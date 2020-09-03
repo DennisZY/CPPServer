74a7d27a9bbc657164c05b38fac69445551947f5
DennisZY <694966299@qq.com>
2020-09-03 13:30:22 +0800

    up

--- Src/DaemonRun.cpp
+++ Src/DaemonRun.cpp
@@ -1,9 +1,10 @@
-#include "CommonHead.h"
 #include "DaemonRun.h"
+#include "CommonHead.h"
 void daemon_run()
 {
-    std::cout<<"daemon run"<<std::endl;
+    std::cout << "daemon run" << std::endl;
     int pid;
+    // 忽略子进程信号
     signal(SIGCHLD, SIG_IGN);
     //1）在父进程中，fork返回新创建子进程的进程ID；
     //2）在子进程中，fork返回0；
@@ -15,14 +16,16 @@ void daemon_run()
         exit(-1);
     }
     //父进程退出，子进程独立运行
-    else if (pid > 0) {
+    else if (pid > 0)
+    {
         exit(0);
     }
     //之前parent和child运行在同一个session里,parent是会话（session）的领头进程,
     //parent进程作为会话的领头进程，如果exit结束执行的话，那么子进程会成为孤儿进程，并被init收养。
     //执行setsid()之后,child将重新获得一个新的会话(session)id。
     //这时parent退出之后,将不会影响到child了。
-    setsid();    
+    setsid();
+    // 将标准输入标准输出和标准错误重定向到/dev/null
     int fd = open("/dev/null", O_RDWR, 0);
     if (fd != -1)
     {
